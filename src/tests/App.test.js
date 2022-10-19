3f77b27fc509fb253989fe4699475b295913e12a
Túlio Mendes <tulioalcan@gmail.com>
2022-10-19 15:44:44 -0300

    criando renderWhitRouter e se a aplicação possuí conjunto de links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente App.js', () => {
+  test('Verifica se a aplicação tem um conjunto fixo de links', () => {
+    renderWithRouter(<App />);
+
+    const link = screen.getAllByRole('link', { name: /Home/i });
+    expect(link[0]).toBeInTheDocument();
+
+    const link2 = screen.getAllByRole('link', { name: /About/i });
+    expect(link2[0]).toBeInTheDocument();
+
+    const link3 = screen.getAllByRole('link', { name: /Favorite Pokémons/i });
+    expect(link3[0]).toBeInTheDocument();
+  });
+});
