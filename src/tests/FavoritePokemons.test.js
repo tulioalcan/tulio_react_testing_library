427f8cc706539367894a678d35ff2e6938fb6f78
Túlio Mendes <tulioalcan@gmail.com>
2022-10-19 17:15:20 -0300

    requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente FavoritePokemons.js', () => {
+  test('Verifica se é exibido na tela a mensagem No favorite pokemon', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const favorite = screen.getByText(/No favorite pokemon found/i);
+    expect(favorite).toBeInTheDocument();
+  });
+});
